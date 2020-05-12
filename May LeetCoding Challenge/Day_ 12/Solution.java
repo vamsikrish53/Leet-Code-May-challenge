e5f9983f7ceb043c1bf2f6d9512d61d0ff7781e5
Krish <vamsikrish.49@gmail.com>
2020-05-12 23:08:20 +0530

    Day 12 Solution

---
+++ May LeetCoding Challenge/Day_ 12/Solution.java
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/single-element-in-a-sorted-array/
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        for(int i=0; i<nums.length-1; i = i+2)
+            if(nums[i] != nums[i+1])
+                return nums[i];
+
+        return nums[nums.length-1];
+    }
+}
