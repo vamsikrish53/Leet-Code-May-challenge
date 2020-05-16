566c0aaf1c002607735baa3ed3daa7d7aef04d62
Krish <vamsikrish.49@gmail.com>
2020-05-16 23:47:30 +0530

    Day 16 solution

--- May LeetCoding Challenge/Day_ 16/Solution.java
+++ May LeetCoding Challenge/Day_ 16/Solution.java
@@ -1,1 +1,38 @@
+// https://leetcode.com/problems/odd-even-linked-list/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 
+class Solution {
+  // Two pointer approach
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        
+        ListNode odd = head;
+        ListNode h_odd = head;
+        ListNode even = head.next;
+        ListNode h_even = head.next;
+        
+        while(even != null) {
+            if(even.next == null) {
+                odd.next = h_even;
+                return h_odd;
+            }
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even= even.next;
+        }
+        odd.next = h_even;
+        
+        return h_odd;
+    }
+}
